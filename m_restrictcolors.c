09b08115590c491fedfd81e2c7c0ae72472f53f1
hyperi0n <oxmoz.zaroc@gmail.com>
2012-06-24 12:39:05 +0300

    Fork no_color

--- m_restrictcolors.c
+++ m_restrictcolors.c
@@ -1,12 +1,16 @@
 /*
- *   IRC - Internet Relay Chat, m_restrictcolors.c
- *   (C) 2012 CrazyCat
- *
- * This module allow the +W mode on a channel
+ * This module allow the +d mode on a channel
  * When activated, only halfops and ops can use
  * colors on the chan.
  * Others are stripped.
  */
+
+#include <fcntl.h>
+#include <time.h>
+#include <sys/stat.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 #include "config.h"
 #include "struct.h"
@@ -14,81 +18,78 @@
 #include "sys.h"
 #include "numeric.h"
 #include "msg.h"
+#include "proto.h"
 #include "channel.h"
-#include <time.h>
-#include <sys/stat.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include "h.h"
+
 #ifdef _WIN32
 #include <io.h>
-#endif
-#include <fcntl.h>
-#include "h.h"
-#include "proto.h"
-#ifdef STRIPBADWORDS
-#include "badwords.h"
-#endif
-#ifdef _WIN32
 #include "version.h"
 #endif
 
-Cmode_t RESTRICT_COLORS = 0L;
-Cmode *ModeRcolors = NULL;
+#ifdef STRIPBADWORDS
+#include "badwords.h"
+#endif
 
-ModuleHeader MOD_HEADER(restrictcolors)
- = {
-   "m_restrictcolors",
-   "1.0",
-   "color stripping for non-(half)op",
-   "3.2-b8-1",
-   NULL
-};
+Cmode_t NOCOLOR_BLOCK = 0L;
+Cmode *ModeBlock = NULL;
 
-static ModuleInfo RestrictColorsModInfo;
+ModuleHeader MOD_HEADER(m_nocaps)
+  = {
+	"m_nocolor",
+	"1.1",
+	"Block color",
+	"3.2-b8-1",
+	NULL 
+    };
+
+static ModuleInfo NoColorModInfo;
 static Hook *CheckMsg;
 
-DLLFUNC char *restrictcolors_checkmsg(aClient *, aClient *, aChannel *, char *, int);
+DLLFUNC char *nocolor_checkmsg(aClient *, aClient *, aChannel *, char *, int);
 
-DLLFUNC int MOD_INIT(m_restrictcolors)(ModuleInfo *modinfo)
+DLLFUNC int MOD_INIT(m_nocolor)(ModuleInfo *modinfo)
 {
     ModuleSetOptions(modinfo->handle, MOD_OPT_PERM);
     CmodeInfo req;
-    ircd_log(LOG_ERROR, "debug: mod_init called from m_restrictcolors");
-    sendto_realops("loading m_restrictcolors");
+    ircd_log(LOG_ERROR, "debug: mod_init called from m_nocolor");
+    sendto_realops("loading m_nocolor");
     memset(&req, 0, sizeof(req));
     req.paracount = 0;
-    req.is_ok = extcmode_default_requirehalfop;
-    req.flag = 'W';
-    ModeRcolors = CmodeAdd(modinfo->handle, req, &RESTRICT_COLORS);
+    req.is_ok = extcmode_default_requirechop;
+    req.flag = 'd';
+    ModeBlock = CmodeAdd(modinfo->handle, req, &NOCOLOR_BLOCK);
 
-    bcopy(modinfo,&RestrictColorsModInfo,modinfo->size);
-    CheckMsg = HookAddPCharEx(RestrictColorsModInfo.handle, HOOKTYPE_CHANMSG, restrictcolors_checkmsg);
+    bcopy(modinfo,&NoColorModInfo,modinfo->size);
+    CheckMsg = HookAddPCharEx(NoColorModInfo.handle, HOOKTYPE_CHANMSG, nocolor_checkmsg);
     return MOD_SUCCESS;
 }
 
-DLLFUNC int MOD_LOAD(m_restrictcolors)(int module_load)
+DLLFUNC int MOD_LOAD(m_nocolor)(int module_load)
 {
 	return MOD_SUCCESS;
 }
 
-DLLFUNC int MOD_UNLOAD(m_restrictcolors)(int module_unload)
+DLLFUNC int MOD_UNLOAD(m_nocolor)(int module_unload)
 {
 	HookDel(CheckMsg);
-	CmodeDel(ModeRcolors);
-   ircd_log(LOG_ERROR, "debug: mod_unload called from m_restrictcolors");
-	sendto_realops("unloading m_restrictcolors");
+	CmodeDel(ModeBlock);
+        ircd_log(LOG_ERROR, "debug: mod_unload called from m_nocolor");
+	sendto_realops("unloading m_nocolor");
 	return MOD_SUCCESS;
 }
 
-
-DLLFUNC char *restrictcolors_checkmsg(aClient *cptr, aClient *sptr, aChannel *chptr, char *text, int notice)
+DLLFUNC char *nocolor_checkmsg(aClient *cptr, aClient *sptr, aChannel *chptr, char *text, int notice)
 {
-    if (IsULine(sptr) || IsServer(sptr))
-       return text;
+	if (IsULine(sptr) || IsServer(sptr)) {
+		return text;
+	}
 
-    if (chptr->mode.extmode && RESTRICT_COLORS && !is_chan_op(sptr, chptr) && !is_halfop(sptr, chptr))
-       return StripColors(text);
-
-    return text;
+	if (chptr->mode.extmode & NOCOLOR_BLOCK && !is_chan_op(sptr, chptr) && !is_halfop(sptr, chptr))
+	{
+		return StripColors(text);		    
+	}
+	else {
+		return text;
+	}    
 }
